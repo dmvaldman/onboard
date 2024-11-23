59465f7cf8d5b0362a46e916b36dce538c1ac9f1
David Valdman <dmvaldman@gmail.com>
2024-11-22 19:56:04 -0800

    Add notion to main test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from comms.slack import SlackBot
+from comms.notion import NotionBot
 
 # from agents.agent import Agent
 # from tools.employeeOS import EmployeeOS
@@ -6,13 +7,19 @@ from comms.slack import SlackBot
 from agents.agent_autogen import Agent
 from tools.employeeOS_autogen import EmployeeOS
 
+# Agent
 agent = Agent(
     "AI Analyst",
     "I am an senior data analyst here to help you answer questions. My responses may use markdown, but no other syntax, like latex, mathml, etc."
 )
-
 employee = EmployeeOS(agent)
+
+# Comms
 slackbot = SlackBot()
+notionbot = NotionBot()
 
 slackbot.message_handler = employee
+notionbot.message_handler = employee
+
 slackbot.start()
+notionbot.start()
